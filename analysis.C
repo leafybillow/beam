28814a315320673d52595f3a70bef422c20c63c5
taoye <taoye@mocha.physics.sunysb.edu>
2019-01-17 16:21:32 -0500

    An example of analysis script

--- analysis.C
+++ analysis.C
@@ -1,10 +1,5 @@
-
-#define N_GEM 2; // Number of GEMS
+#include "analysis_rms.h"
 #define THRESHOLD 3.0; // threshold for signal, e.g. 3 means 3*sigma 
-#define N_CH_CMN 100;  // Number of strips used for common mode noise calculation
-#define PITCH 400.0;   // pitch width, unit: um
-#define CUT_MPL 1;     // cut on multiplicity, reject single wire hits (most likely noises or cross-talks)
-#define CUT_COR 1.0;     // cut on correlation, based on correlation studies
 
 Int_t analysis(TString filename="test.root", Bool_t isDebug = 0){
   
@@ -13,117 +8,179 @@ Int_t analysis(TString filename="test.root", Bool_t isDebug = 0){
   TFile* rf_raw = TFile::Open(filename.Data());
   TTree* tree_raw = (TTree*)rf_raw->Get("T");
   cout << "Raw ROOTFile " << filename << " is opened. " << endl;
-
-  TString ped_filename = filename.ReplaceAll(".root","_ped.root");
-  TFile* rf_ped = TFile::Open(ped_filename.Data());
-  cout << "Pedestal ROOTFile " << ped_filename << " is opened. " << endl;
-
-  TString output_filename = filename.ReplaceAll("_ped","_analyzed");
-  TFile* rf_rec = TFile::Open(output_filename,"RECREATE");
+  Ssiz_t first_t = filename.Last('/') +1; // if a slash is not there, return 0.
+  Ssiz_t last_t = filename.Last('.');
+  Int_t length_t = last_t - first_t;
+  TString prefix_t = filename(first_t,length_t);
+  TString output_filename = prefix_t + "_analyzed.root";
+  TFile* rf_rec = TFile::Open("rootfiles/"+output_filename,"RECREATE");
   cout << "ROOTFile " << output_filename << " is recreated. " << endl;
+  //__________________________________________________________________________________
+  // New Reconstructed Tree
+  TTree* tree_rec = new TTree("Rec","Rec"); 
+  //And Build  Reconstruction Branch
 
-  // Create Trees and Branches
-  TTree* tree_rec = new TTree("Rec","Rec"); // Reconstructed Tree
-  
-  BeamGEMPlane* gemPlane1 = new BeamGEMPlane();
-  BeamGEMPlane* gemPlane2 = new BeamGEMPlane();
-  BeamGEMTracker* tracker = new BeamGEMTracker();
+  vector <double> vCharge_x1, vCharge_y1;
+  vector <double> vPosition_x1, vPosition_y1;
+  vector <double> vWidth_x1, vWidth_y1;
+
+  vector <double> vCharge_x2, vCharge_y2;
+  vector <double> vPosition_x2, vPosition_y2;
+  vector <double> vWidth_x2, vWidth_y2;
+
+  Int_t nHits_1, nHits_2;
+
   struct QDC{
-    double us_lo;
-    double us_hi;
-    double ds_lo;
-    double ds_hi;
+    double us_lo; // upstream detector , low range, hi-sensitivity
+    double us_hi; // upstream detector, high range,
+    double ds_lo; // downstream detector
+    double ds_hi; // downstream detector
   }qdc;
 
   TBranch* branch_qdc = tree_rec->Branch("qdc",&qdc,"us_lo/D:us_hi:ds_lo:ds_hi");
-  TBranch* branch_gem1 = tree_rec->Branch("gem1",&gemPlane1);
-  TBranch* branch_gem2 = tree_rec->Branch("gem2",&gemPlane2);
-  TBranch* branch_track = tree_rec->Branch("tracker",&tracker);
-  //_________________________________________________________________________________________
 
-  // GEM common Variables
-  const int ngem = N_GEM;
-  const int napv = 6;  // number of apvs for each GEM
-  const int nch = 128; // number of channel on each APV cards
+  tree_rec->Branch("gem1.charge_x",&vCharge_x1);
+  tree_rec->Branch("gem1.charge_y",&vCharge_y1);
+  tree_rec->Branch("gem1.position_x",&vPosition_x1);
+  tree_rec->Branch("gem1.position_y",&vPosition_y1);
+  tree_rec->Branch("gem1.width_x",&vWidth_x1);
+  tree_rec->Branch("gem1.width_y",&vWidth_y1);
+  tree_rec->Branch("gem1.nHits",&nHits_1);
 
-  TString apv_tag[napv] = {"x","x",
-			   "y","y","y","y"};
-  Int_t base[napv] = {0, 128, 
-		      0, 128 , 256, 384};
-  Double_t cmn[ngem][napv];
-  Int_t nch_cmn = N_CH_CMN; // number of channels used for computing  common mode
+  tree_rec->Branch("gem2.charge_x",&vCharge_x2);
+  tree_rec->Branch("gem2.charge_y",&vCharge_y2);
+  tree_rec->Branch("gem2.position_x",&vPosition_x2);
+  tree_rec->Branch("gem2.position_y",&vPosition_y2);
+  tree_rec->Branch("gem2.width_x",&vWidth_x2);
+  tree_rec->Branch("gem2.width_y",&vWidth_y2);
+  tree_rec->Branch("gem2.nHits",&nHits_2);
 
-  //_________________________________________________________________________________________
-  
+  //__________________________________________________________________________________
+  // GEM Configuration Parameters
+
+  Int_t nProj = 4; // number of projections, 2 GEM *(X+Y) = 4
+  Int_t nadc = 6; // number of adc samples
+
+  TString strADC[6]={"adc0","adc1","adc2","adc3","adc4","adc5"};
+  TString strProj[4]={"x1","y1","x2","y2"};
+  Int_t sizeArray[4]={256, 512, 256, 512}; 
+  //__________________________________________________________________________________
+  // Initialize EventReader for Raw Tree
+  Double_t* rms[4]={rms_x1,rms_y1,rms_x2,rms_y2};
+
   Double_t us_hi,us_lo,ds_hi,ds_lo; // dummy variables
-  tree_raw->SetBranchAddress("sbs.sbuscint.hadc0",&us_hi);
-  tree_raw->SetBranchAddress("sbs.sbuscint.ladc0",&us_lo);
+  // Note: qdc channels need to be changed depending on run configuration
+  // Here just randomly pick up two channels for demonstration
+  tree_raw->SetBranchAddress("sbs.sbuscint.hadc2",&us_hi);
+  tree_raw->SetBranchAddress("sbs.sbuscint.ladc2",&us_lo);
   tree_raw->SetBranchAddress("sbs.sbuscint.hadc1",&ds_hi);
   tree_raw->SetBranchAddress("sbs.sbuscint.ladc1",&ds_lo);
+
+  BeamGEMData bgData[4];  // GEM Data Containers
+
+  for(Int_t iProj=0;iProj<nProj;iProj++){
+
+    bgData[iProj] = BeamGEMData(iProj,sizeArray[iProj]);
+
+    for(Int_t iadc=0;iadc<nadc;iadc++){
+      TString strBranch = Form("sbs.gems.%s.%s",
+			       strProj[iProj].Data(),
+			       strADC[iadc].Data());
+      tree_raw->SetBranchAddress(strBranch, bgData[iProj].adc[iadc]);
+    }
+    tree_raw->SetBranchAddress(Form("sbs.gems.%s.strip",strProj[iProj].Data()),
+			       bgData[iProj].id_strip);
+    tree_raw->SetBranchAddress(Form("sbs.gems.%s.nch",strProj[iProj].Data()),
+			       &(bgData[iProj].nChannel));
+    tree_raw->SetBranchAddress(Form("sbs.gems.%s.adc_sum",strProj[iProj].Data()),
+			       bgData[iProj].adc_sum);
+    tree_raw->SetBranchAddress(Form("sbs.gems.%s.common_mode",strProj[iProj].Data()),
+			       bgData[iProj].common_mode);
+  }
+  //__________________________________________________________________________________
+
   //*Event loop, reconstruction
+
+  BeamGEMProjection* bgProjection[4];
+
   Int_t nentries = tree_raw->GetEntries();
-  for(int ievt=0;ievt<nentries;ievt++){
+  for(Int_t ievt=0;ievt<nentries;ievt++){
+    if(ievt%200==0)
+      cout << ievt << " events analyzed"  << endl;
     tree_raw->GetEntry(ievt);
-    // -- Analyze Detector/QDC signal
-    //** Get QDC from both low and high range channels, and upstream and downstream detectors
+
+    //** Retrieve replayed data from the raw tree
+    //*** QDC
     qdc.us_lo = us_lo;
     qdc.us_hi = us_hi;
     qdc.ds_lo = ds_lo;
     qdc.ds_hi = ds_hi;
-    // -- Now, Analyze GEM 
 
-    //** Retrieve replayed data from the raw tree
-    for(int igem=0; igem<2; igem++){
-      for(int iapv=0; iapv<napv; iapv++){
-	for(int ich=0; ich<nch; ich++){
+    //*** GEM
+    for(Int_t iProj=0;iProj<nProj;iProj++){
+      Int_t nChannel = (Int_t)bgData[iProj].nChannel;
+
+      bgProjection[iProj] = new BeamGEMProjection( strProj[ bgData[iProj].id_Proj ],
+						   (Int_t)bgData[iProj].nChannel);
+      for(Int_t ich=0; ich<nChannel;ich++){
 	  
+	Double_t arADC[6];  	  // *** A Buffer container ADC Samples
+	// common mode correction here
+	for(Int_t iadc=0 ;iadc<nadc;iadc++){
+	  arADC[iadc] = bgData[iProj].adc[iadc][ich] - bgData[iProj].common_mode[ich];
+	} 
+	Int_t myStripID = (Int_t)bgData[iProj].id_strip[ich];
+	BeamGEMStrip* bgStrip = new BeamGEMStrip(arADC,myStripID);
+	// effectively zero suppression
+	if(bgStrip->GetADCsum()>THRESHOLD*sqrt(6)*rms[iProj][ich]){ 
+	  bgStrip->Process();
+	  bgProjection[iProj]->AddStrip(bgStrip);
 	}
-      }
-    }
-    //** Calculate Common Mode 
+      } // End channel loop
+      bgProjection[iProj]->Process();
+    } // End Projection Loop
 
-    //** Do Pedestal and Common Mode correction 
-    //** Calculate amplitude from each stripes and fill into a 1D histogram
-    //** SearchHits in each readout coordinate from the 1D histogram
-    //*** TestSplit 
-    //** Verify  nhits match betweeen two readout coordinate
-    //** Calculate cluster charge in 1D
-    //** Calculate Multiplicity in 1D 
-    //** Calculate Position in 1D , using centroid for now
-    //** Reconstruct 2D hits from correlation
-    //** Build Tracks
-    //*** Sort second GEM hits array to match to first GEM.
-    //*** Calculate slopes
-    //*** Calculate angles
-    //*** Option: Plot Tracks
+    BeamGEMPlane *bgPlane1 = new BeamGEMPlane("gem1");
+    BeamGEMPlane *bgPlane2 = new BeamGEMPlane("gem2");
+    bgPlane1->AddProjectionX(bgProjection[0]);
+    bgPlane1->AddProjectionY(bgProjection[1]);
+    bgPlane2->AddProjectionX(bgProjection[2]);
+    bgPlane2->AddProjectionY(bgProjection[3]);
+    bgPlane1->Process();
+    bgPlane2->Process();
 
-    //** Fill this entry
+    vCharge_x1 = bgPlane1->GetChargeX();
+    vCharge_y1 = bgPlane1->GetChargeY();
+    vPosition_x1 = bgPlane1->GetPositionX();
+    vPosition_y1 = bgPlane1->GetPositionY();
+
+    vWidth_x1 = bgPlane1->GetWidthX();
+    vWidth_y1 = bgPlane1->GetWidthY();
+
+    nHits_1 = bgPlane1->GetNHits();
+
+    vCharge_x2 = bgPlane2->GetChargeX();
+    vCharge_y2 = bgPlane2->GetChargeY();
+    vPosition_x2 = bgPlane2->GetPositionX();
+    vPosition_y2 = bgPlane2->GetPositionY();
+
+    vWidth_x2 = bgPlane2->GetWidthX();
+    vWidth_y2 = bgPlane2->GetWidthY();
+
+    nHits_2 = bgPlane2->GetNHits();
+
+    // if(nHits_1==1 && qdc.us_hi>1500){
+    //   bgPlane1->PlotResults(prefix_t,ievt);
+    // }
+
     tree_rec->Fill();
-    //** Option: output plots
-  }  // End Event loop
-  //_________________________________________________________________________________________
+  } // End Event loop
+
+  //__________________________________________________________________________________
+  
   rf_raw->Close();
-  rf_ped->Close();
   tree_rec->Write();
   rf_rec->Close();
+
   return 0;
 }
-
-// User Define Functions
-
-Double_t GetCommonMode(Int_t *arr_integral){
-}
-
-Double_t GetStripeAmpl(Int_t *arr_sample, Double_t ped ){
-  return ampl;
-}
-
-TH1D* GetHistX(){
-  return hist_x;
-}
-
-TH1D* GetHistY(){
-  return hist_y;
-}
-
-
