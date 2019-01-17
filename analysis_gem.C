1a76919b5cb96170170f1628ecbc0487ed792fc0
taoye <taoye@mocha.physics.sunysb.edu>
2019-01-17 13:43:46 -0500

    Implement BeamGEMData in analysis

--- analysis_gem.C
+++ analysis_gem.C
@@ -8,9 +8,12 @@ Int_t analysis_gem(TString filename="test.root", Bool_t isDebug = 0){
   TFile* rf_raw = TFile::Open(filename.Data());
   TTree* tree_raw = (TTree*)rf_raw->Get("T");
   cout << "Raw ROOTFile " << filename << " is opened. " << endl;
-  TString runName = filename.ReplaceAll(".root",""); // I need it .
+  Ssiz_t first_t = filename.Last('/') +1; // if a slash is not there, return 0.
+  Ssiz_t last_t = filename.Last('.');
+  Int_t length_t = last_t - first_t;
+  TString prefix_t = filename(first_t,length_t);
   //__________________________________________________________________________________
-  TString output_filename = runName + "_analyzed.root";
+  TString output_filename = prefix_t + "_analyzed.root";
   TFile* rf_rec = TFile::Open(output_filename,"RECREATE");
   cout << "ROOTFile " << output_filename << " is recreated. " << endl;
   // Create Trees 
@@ -18,10 +21,16 @@ Int_t analysis_gem(TString filename="test.root", Bool_t isDebug = 0){
   //And Build  Reconstruction Branch
   vector <double> vCharge_x, vCharge_y;
   vector <double> vPosition_x, vPosition_y;
+  vector <double> vWidth_x, vWidth_y;
+  Int_t nHits;
   tree_rec->Branch("charge_x",&vCharge_x);
   tree_rec->Branch("charge_y",&vCharge_y);
-  tree_rec->Branch("position_x",&vCharge_x);
-  tree_rec->Branch("position_y",&vCharge_y);
+  tree_rec->Branch("position_x",&vPosition_x);
+  tree_rec->Branch("position_y",&vPosition_y);
+  tree_rec->Branch("width_x",&vWidth_x);
+  tree_rec->Branch("width_y",&vWidth_y);
+
+  tree_rec->Branch("nHits",&nHits);
   //__________________________________________________________________________________
   // GEM Configuration Parameters
 
@@ -33,88 +42,64 @@ Int_t analysis_gem(TString filename="test.root", Bool_t isDebug = 0){
   Int_t sizeArray[4]={256, 512, 256, 512}; 
   //__________________________________________________________________________________
   // Initialize EventReader for Raw Tree
-  struct StcProjection{
-    // containers for data
-    Double_t* adc[6];
-    Double_t* adc_sum;
-    Double_t* common_mode;
-    Double_t* id_strip; // channel-to-strip mapping array
-    Double_t nChannel;  // Note: nch in SBS-offline decoder is a float number
-
-    // identification for projection
-    Int_t id_Proj; // starts from 0 to 3; {"x1","y1","x2","y2"}
-  };
   Double_t* rms[4]={rms_x1,rms_y1,rms_x2,rms_y2};
-  struct StcProjection Proj[4];
+
+  BeamGEMData bgData[4];  // GEM Data Containers
 
   for(Int_t iProj=0;iProj<nProj;iProj++){
 
-    Proj[iProj].id_strip = new Double_t[ sizeArray[iProj] ];
-    Proj[iProj].adc_sum = new Double_t[ sizeArray[iProj] ];
-    Proj[iProj].common_mode = new Double_t[ sizeArray[iProj] ];
-    Proj[iProj].id_Proj = iProj;
+    bgData[iProj] = BeamGEMData(iProj,sizeArray[iProj]);
 
     for(Int_t iadc=0;iadc<nadc;iadc++){
-      Proj[iProj].adc[iadc] = new Double_t[ sizeArray[iProj] ];
       TString strBranch = Form("sbs.gems.%s.%s",
 			       strProj[iProj].Data(),
 			       strADC[iadc].Data());
-      tree_raw->SetBranchAddress(strBranch, Proj[iProj].adc[iadc]);
+      tree_raw->SetBranchAddress(strBranch, bgData[iProj].adc[iadc]);
     }
     tree_raw->SetBranchAddress(Form("sbs.gems.%s.strip",strProj[iProj].Data()),
-			       Proj[iProj].id_strip);
-
+			       bgData[iProj].id_strip);
     tree_raw->SetBranchAddress(Form("sbs.gems.%s.nch",strProj[iProj].Data()),
-			       &(Proj[iProj].nChannel));
+			       &(bgData[iProj].nChannel));
     tree_raw->SetBranchAddress(Form("sbs.gems.%s.adc_sum",strProj[iProj].Data()),
-			       Proj[iProj].adc_sum);
+			       bgData[iProj].adc_sum);
     tree_raw->SetBranchAddress(Form("sbs.gems.%s.common_mode",strProj[iProj].Data()),
-			       Proj[iProj].common_mode);
-
-
+			       bgData[iProj].common_mode);
   }
   //__________________________________________________________________________________
 
   //*Event loop, reconstruction
-  Int_t nentries = 50;//tree_raw->GetEntries();
-  for(Int_t ievt=0;ievt<nentries;ievt++){
-    tree_raw->GetEntry(ievt);
 
-    BeamGEMProjection* bgProjection[4];
+  BeamGEMProjection* bgProjection[4];
+
+  Int_t nentries = tree_raw->GetEntries();
+  for(Int_t ievt=0;ievt<nentries;ievt++){
+    if(ievt%200==0)
+      cout << ievt << " events analyzed"  << endl;
+    tree_raw->GetEntry(ievt);
 
     //** Retrieve replayed data from the raw tree
     for(Int_t iProj=0;iProj<nProj;iProj++){
-      Int_t nChannel = (Int_t)Proj[iProj].nChannel;
-      if ( nChannel != sizeArray[iProj]){       // ***Check number of Channel 
-	std::cout << __LINE__ << ":"
-		  << "Error: " 
-		  << "number of channels  mistached "
-		  << " on Projection " << strProj[iProj]<< std::endl;
-	std::cout << sizeArray[iProj] << " is expected." << std::endl;
-	std::cout << nChannel << " is found." << std::endl;
-	break;
-      }
-      else{  
-	bgProjection[iProj] = new BeamGEMProjection( strProj[ Proj[iProj].id_Proj ],
-								 (Int_t)Proj[iProj].nChannel);
-	for(Int_t ich=0; ich<nChannel;ich++){
-	  
+      Int_t nChannel = (Int_t)bgData[iProj].nChannel;
 
-	    Double_t arADC[6];  	  // *** Transpose ADC Samples
-	    for(Int_t iadc=0 ;iadc<nadc;iadc++){
-	      arADC[iadc] = Proj[iProj].adc[iadc][ich] - Proj[iProj].common_mode[ich];
-	    } // End sample loop
-	    Int_t myStripID = (Int_t)Proj[iProj].id_strip[ich];
-	    BeamGEMStrip* bgStrip = new BeamGEMStrip(arADC,myStripID);
-	    // Effectively zero suppression
-	    if(bgStrip->GetRawAmplitude()>THRESHOLD*rms[iProj][ich]){ 
-	      bgStrip->Process();
-	      bgProjection[iProj]->AddStrip(bgStrip);
-	    }
-	} // End channel loop
-	bgProjection[iProj]->Process();
-	// bgProjection[iProj]->PlotResults(runName,ievt);
-      } // End number of channel check
+      bgProjection[iProj] = new BeamGEMProjection( strProj[ bgData[iProj].id_Proj ],
+						   (Int_t)bgData[iProj].nChannel);
+      for(Int_t ich=0; ich<nChannel;ich++){
+	  
+	Double_t arADC[6];  	  // *** A Buffer container ADC Samples
+	// common mode correction here
+	for(Int_t iadc=0 ;iadc<nadc;iadc++){
+	  arADC[iadc] = bgData[iProj].adc[iadc][ich] - bgData[iProj].common_mode[ich];
+	} 
+	Int_t myStripID = (Int_t)bgData[iProj].id_strip[ich];
+	BeamGEMStrip* bgStrip = new BeamGEMStrip(arADC,myStripID);
+	// // effectively zero suppression
+	if(bgStrip->GetADCsum()>THRESHOLD*sqrt(6)*rms[iProj][ich]){ 
+	  bgStrip->Process();
+	  bgProjection[iProj]->AddStrip(bgStrip);
+	}
+      } // End channel loop
+      bgProjection[iProj]->Process();
+      // bgProjection[iProj]->PlotResults(prefix_t,ievt);
     } // End Projection Loop
 
     BeamGEMPlane *bgPlane1 = new BeamGEMPlane("gem1");
@@ -123,11 +108,23 @@ Int_t analysis_gem(TString filename="test.root", Bool_t isDebug = 0){
     bgPlane1->AddProjectionY(bgProjection[1]);
     bgPlane2->AddProjectionX(bgProjection[2]);
     bgPlane2->AddProjectionY(bgProjection[3]);
+    bgPlane1->Process();
 
-    bgPlane1->PlotResults(runName,ievt);
-    bgPlane2->PlotResults(runName,ievt);
+    vCharge_x = bgPlane1->GetChargeX();
+    vCharge_y = bgPlane1->GetChargeY();
+    vPosition_x = bgPlane1->GetPositionX();
+    vPosition_y = bgPlane1->GetPositionY();
+
+    vWidth_x = bgPlane1->GetWidthX();
+    vWidth_y = bgPlane1->GetWidthY();
+
+    nHits = bgPlane1->GetNHits();
+    // if(nHits>0 && fabs(vCharge_x[0]-vCharge_y[0]-1200)>3000){
+    //   bgPlane1->PlotResults(prefix_t,ievt);
+    //   bgPlane2->PlotResults(prefix_t,ievt);
+    // }
     //** Fill this entry
-    //tree_rec->Fill();
+    tree_rec->Fill();
   } // End Event loop
 
   //__________________________________________________________________________________
