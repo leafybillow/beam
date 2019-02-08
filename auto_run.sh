5da01a0ef82047ddf81064fa21227c6030858ca7
leafybillow <yetao@debian>
2019-02-08 10:44:29 -0500

    auto run scripts

--- auto_run.sh
+++ auto_run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 run_num=$1
-file=rootfiles/run_$run_num.root
+ln -sf DBfiles/db_sbs.gems.dat_oneMPD_noped db_sbs.gems.dat
+analyzer -b -q 'replay.C('$run_num')' ;
+./beam -t ped  -r $run_num;
+ln -sf DBfiles/db_sbs.gems.dat_run_$run_num db_sbs.gems.dat
+analyzer -b -q 'replay.C('$run_num')' ;
+./beam -t rms -r $run_num;
+cp DBfiles/analysis_rms.h_run_$run_num table_rms.h
+./beam -r $run_num
 
-ln -sf DBfiles/db_sbs.gems.dat_noped db_sbs.gems.dat ;
-analyzer -b -q 'replay.C('$run_num')' ;
-analyzer -b -q 'analysis_ped.C( "'$file'")';
-analyzer -b -q 'replay.C('$run_num')' ;
-analyzer -b -q 'analysis_rms.C("'$file'")' ;
-cp DBfiles/analysis_rms.h_run_$run_num analysis_rms.h
-analyzer -b -q 'analysis.C("'$file'")';
