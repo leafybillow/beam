1c01ee4c416551c7cdfef82f0c2316293ff3629f
leafybillow <tao.ye@stonybrook.edu>
2019-04-01 13:58:41 -0400

    Updated auto-run script

--- replay/auto_run.sh
+++ replay/auto_run.sh
@@ -7,6 +7,5 @@ analyzer -b -q 'replay.C('$run_num')' ;
 ../beam -t ped  -r $run_num -c ../beam.conf
 ln -sf ../DBfiles/db_sbs.gems.dat_run_$run_num db_sbs.gems.dat
 analyzer -b -q 'replay.C('$run_num')' ;
-# ../beam -t rms -r $run_num -c ../beam.conf
 
 
