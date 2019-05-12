fb33568ab0a5df0398d269e86c2976ff374bfeb9
Tao Ye <tao.ye@stonybrook.edu>
2019-05-12 14:11:36 -0400

    Updated config files

--- replay/auto_run.sh
+++ replay/auto_run.sh
@@ -2,9 +2,12 @@
 
 run_num=$1
 ln -sf db_sbs.gems.dat_oneMPD_noped db_sbs.gems.dat
-#ln -sf db_sbs.gems.dat_twoMPD_noped db_sbs.gems.dat
+# ln -sf db_sbs.gems.dat_twoMPD_noped db_sbs.gems.dat
 analyzer -b -q 'replay.C('$run_num')' ;
-../beam -t ped  -r $run_num -c ../beam.conf
+
+../beam -t ped  -r $run_num -c oneMPD.conf
+#../beam -t ped  -r $run_num -c twoMPD.conf
+
 ln -sf ../DBfiles/db_sbs.gems.dat_run_$run_num db_sbs.gems.dat
 analyzer -b -q 'replay.C('$run_num')' ;
 
