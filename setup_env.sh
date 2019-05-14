b1878253f7e8da6e92781cfa446fd7b92655a115
Tao Ye <tao.ye@stonybrook.edu>
2019-05-14 13:12:33 -0400

    Updated setup script for Mocha server

--- setup_env.sh
+++ setup_env.sh
@@ -1,15 +1,17 @@
 
-source /usr/local/root-5/bin/thisroot.sh
-export ANALYZER=/home/yetao/workarea/slac_beam/analyzer
-export EVIO_INCDIR=/home/yetao/workarea/slac_beam/analyzer/evio
-export EVIO_LIBDIR=/home/yetao/workarea/slac_beam/analyzer
-export SBS_LIBDIR=/home/yetao/workarea/slac_beam/SBS-offline/build
+source /share/apps/root-5.34.36-build/bin/thisroot.sh
+
+export ANALYZER=/export/home/slactest/AnalysisTool/analyzer
+export EVIO_INCDIR=/export/home/slactest/AnalysisTool/analyzer/evio
+export EVIO_LIBDIR=/export/home/slactest/AnalysisTool/analyzer/
+export SBS_LIBDIR=/export/home/slactest/AnalysisTool/SBS-offline
+
 export PATH=$PATH:$ANALYZER
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SBS_LIBDIR
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ANALYZER
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$EVIO_LIBDIR
 
-export BEAM_DATA=/home/yetao/workarea/slac_beam/raw_data/
-export BEAM_ROOTFILES=/home/yetao/workarea/slac_beam/rootfiles/
-export BEAM_DB_PATH=/home/yetao/workarea/slac_beam/beam/DBfiles/
+export BEAM_DATA=/export/home/slactest/raw_data/
+export BEAM_ROOTFILES=/export/home/slactest/rootfiles/
+export BEAM_DB_PATH=/export/home/slactest/DBfiles/
