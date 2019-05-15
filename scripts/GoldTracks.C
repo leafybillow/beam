3db247f29bfcf85b0da72804d5d10cc695117239
Tao Ye <tao.ye@stonybrook.edu>
2019-05-15 10:40:34 -0400

    One more interesting plot

--- scripts/GoldTracks.C
+++ scripts/GoldTracks.C
@@ -2,11 +2,16 @@ void GoldTracks(){
   TString rf_path = getenv("BEAM_ROOTFILES");
   TFile* rootfile = TFile::Open(rf_path+"run_419_analyzed.root");
 
-  TCanvas *c1 =new TCanvas("c1","c1",700,700);
+  TCanvas *c1 =new TCanvas("c1","c1",600,600);
   c1->cd();
   Rec->Draw("det1_qdc_hr","!isNoisy && isGoldenTrack");
   TH1D* h_buff = (TH1D*)c1->FindObject("htemp");
   h_buff->SetLineColor(kRed);
+
+
+  TCanvas *c2 =new TCanvas("c2","c2",600,600);
+  c2->cd();
+  Rec->Draw("det1_qdc_hr:det1_x","!isNoisy && isGoldenTrack","prof");
 
 
   TCanvas *c4 =new TCanvas("c4","c4",500,800);
